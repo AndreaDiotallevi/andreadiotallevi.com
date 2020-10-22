e98f9eab887d5d38ff2db53c9256b8bbb38b943c
Andrea Diotallevi <andrea.diotallevi@outlook.com>
2020-10-22 20:22:24 +0100

    complete course part 2

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,32 @@
 module.exports = {
+  siteMetadata: {
+    title: "Andrea Diotallevi | Software Developer",
+    author: "Andrea Diotallevi"
+  },
   plugins: [
-    "gatsby-plugin-sass"
-  ],
+    "gatsby-plugin-sass",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "src",
+        path: `${__dirname}/src/`,
+      }
+    },
+    "gatsby-plugin-sharp",
+    {
+      resolve: "gatsby-transformer-remark",
+      options: {
+        plugins: [
+          "gatsby-remark-relative-images",
+          {
+            resolve: "gatsby-remark-images",
+            options: {
+              maxWidth: 750,
+              linkImagesToOriginal: false
+            }
+          }
+        ]
+      }
+    }
+  ]
 }
---
+++ src/posts/react.md
@@ -0,0 +1,10 @@
+---
+title: "React Blog Post"
+date: "2020-10-16"
+---
+
+I am learning how to create a blog with Gatsby
+
+## Topics Covered
+
+1. Blog
