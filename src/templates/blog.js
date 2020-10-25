5d8cb4bf27518e35e2df34e7efe80b200a9f6edc
Andrea Diotallevi <andrea.diotallevi@outlook.com>
2020-10-25 17:07:19 +0000

    amend blog styles

--- src/templates/blog.js
+++ src/templates/blog.js
@@ -24,13 +24,13 @@ query (
 `
 
 const Blog = (props) => {
-  return (
-    <Layout>
-      <h1>{props.data.markdownRemark.frontmatter.title}</h1>
-      <p>{props.data.markdownRemark.frontmatter.date}</p>
-      <div dangerouslySetInnerHTML={{ __html: props.data.markdownRemark.html }}></div>
-    </Layout>
-  )
+	return (
+		<Layout>
+			<h1>{props.data.markdownRemark.frontmatter.title}</h1>
+			<p>{props.data.markdownRemark.frontmatter.date}</p>
+			<div dangerouslySetInnerHTML={{ __html: props.data.markdownRemark.html }}></div>
+		</Layout>
+	)
 }
 
 export default Blog
