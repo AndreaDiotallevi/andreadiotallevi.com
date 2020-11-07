679f82882dd0066a2a9749a142defd9cd9685214
Andrea Diotallevi <andrea.diotallevi@outlook.com>
2020-11-07 19:52:42 +0000

    add blue fills on blog page

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -75,8 +75,28 @@ const BlogPage = () => {
                                 }}
                             />
                         </div> */}
+                        <div style={{ position: "relative", top: -16, left: -16 }}>
+                            <div
+                                style={{
+                                    position: "absolute",
+                                    width: 43,
+                                    height: 43,
+                                    backgroundColor: "rgba(0, 0, 255, 0.25)",
+                                }}
+                            />
+                        </div>
                         <Link to={`/blog/${edge.node.fields.slug}`}>
                             <h2>{edge.node.frontmatter.title}</h2>
+                            <div style={{ position: "relative", top: -8, left: -16 }}>
+                                <div
+                                    style={{
+                                        position: "absolute",
+                                        width: 8,
+                                        height: 42,
+                                        backgroundColor: "rgba(0, 0, 255, 0.25)",
+                                    }}
+                                />
+                            </div>
                             <p className={blogStyles.date}>{edge.node.frontmatter.date}</p>
                             <div className={blogStyles.flexContainer}>
                                 <div className={blogStyles.descriptionDateDiv}>
--- src/pages/index.js
+++ src/pages/index.js
@@ -43,8 +43,28 @@ const IndexPage = () => {
             <ol className={indexStyles.posts}>
                 {data.allMarkdownRemark.edges.map(edge => (
                     <li className={indexStyles.post} key={edge.node.frontmatter.title}>
+                        <div style={{ position: "relative", top: -16, left: -16 }}>
+                            <div
+                                style={{
+                                    position: "absolute",
+                                    width: 43,
+                                    height: 43,
+                                    backgroundColor: "rgba(0, 0, 255, 0.25)",
+                                }}
+                            />
+                        </div>
                         <Link to={`/blog/${edge.node.fields.slug}`}>
                             <h2>{edge.node.frontmatter.title}</h2>
+                            <div style={{ position: "relative", top: -8, left: -16 }}>
+                                <div
+                                    style={{
+                                        position: "absolute",
+                                        width: 8,
+                                        height: 42,
+                                        backgroundColor: "rgba(0, 0, 255, 0.25)",
+                                    }}
+                                />
+                            </div>
                             <p className={indexStyles.date}>{edge.node.frontmatter.date}</p>
                             <div className={indexStyles.flexContainer}>
                                 <div className={indexStyles.descriptionDateDiv}>
