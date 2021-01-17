e85172b68e1809d8bd3c680630a579a6e9cddc8c
Andrea Diotallevi <andrea.diotallevi@outlook.com>
2021-01-17 10:54:04 +0100

    update about text

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { graphql } from "gatsby"
+import { graphql, Link } from "gatsby"
 import Img from "gatsby-image"
 
 import Layout from "../components/layout"
@@ -39,8 +39,8 @@ const AboutPage = (props) => {
 					<Img fluid={props.data.fileName.childImageSharp.fluid} />
 				</div>
 				<div className={aboutStyles.aboutTextDiv}>
-					<p>I am a full-stack software developer at <a href="https://rippleenergy.com/" target="_blank" rel="noreferrer">Ripple Energy</a> based in London.</p>
-					<p>I enjoy writing about algorithms and software development best practices to improve my skills and share what I learn with our amazing software community.</p>
+					<p>I am a full-stack software developer at <a href="https://rippleenergy.com/" target="_blank" rel="noreferrer">Ripple Energy</a>, where I contribute to the renewable energy transformation allowing people to part-own a new wind farm to power their homes with clean electricity.</p>
+					<p>To improve my skills, I enjoy writing <Link to="/blog">articles</Link> about algorithms and software development best practices sharing what I learn with our great software community.</p>
 					<p>Before moving into software development, I worked as a residential architect for five years, which has helped me every day to communicate with clarity with my teammates and contribute to the company with my design sensibility.</p>
 					<p>As a pianist, music has always been an integral part of my life. The habit of practicing and composing pieces of music has taught me how to concentrate for long hours and build a balanced and grounded mindset.</p>
 				</div>
