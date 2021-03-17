b1ee0e3c5c91cd5d12247811402d10211f2b3c18
Andrea Diotallevi <andrea.diotallevi@outlook.com>
2021-03-17 23:08:10 +0000

    amend navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -7,27 +7,21 @@ import routes from "../utils/routes"
 import navbarStyles from "./navbar.module.scss"
 
 const Navbar = ({ open, onClick }) => {
-    return (
-        <nav className={navbarStyles.navbar}>
-            <div>
-                {!open && (
-                    <Link to="/">
-                        andreadiotallevi.com
-                    </Link>
-                )}
-            </div>
-            <ul>
-                {routes.map(({ menuName, baseUrl }) => (
-                    <li key={menuName}>
-                        <Link to={baseUrl} activeClassName={navbarStyles.activeNavItem}>
-                            {menuName}
-                        </Link>
-                    </li>
-                ))}
-            </ul>
-            <MenuToggleButton open={open} onClick={onClick} />
-        </nav>
-    )
+  return (
+    <nav className={navbarStyles.navbar}>
+      <div>{!open && <Link to="/">andreadiotallevi</Link>}</div>
+      <ul>
+        {routes.map(({ menuName, baseUrl }) => (
+          <li key={menuName}>
+            <Link to={baseUrl} activeClassName={navbarStyles.activeNavItem}>
+              {menuName}
+            </Link>
+          </li>
+        ))}
+      </ul>
+      {/* <MenuToggleButton open={open} onClick={onClick} /> */}
+    </nav>
+  )
 }
 
 export default Navbar
