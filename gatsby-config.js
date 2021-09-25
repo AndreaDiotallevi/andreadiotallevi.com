e54a1d5bafb897a28f9c699f43a65828181f9fae
Andrea Diotallevi <andrea.diotallevi@outlook.com>
2021-09-25 15:03:09 +0200

    add config for mailchimp

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,64 +1,70 @@
 require(`dotenv`).config({
-	path: `.env.${process.env.NODE_ENV}`,
+    path: `.env.${process.env.NODE_ENV}`,
 })
 
 module.exports = {
-	siteMetadata: {
-		title: `Andrea Diotallevi | Software Developer`,
-		author: `Andrea Diotallevi`,
-		description: `Andrea Diotallevi's personal website`,
-		url: `https://www.andreadiotallevi.com`,
-		image: `/andrea-diotallevi.jpg`,
-		twitterUsername: `@a_diotallevi_`
-	},
-	plugins: [
-		`gatsby-plugin-react-helmet`,
-		`gatsby-plugin-sass`,
-		{
-			resolve: `gatsby-source-filesystem`,
-			options: {
-				name: `src`,
-				path: `${__dirname}/src/`,
-			}
-		},
-		`gatsby-plugin-sharp`,
-		`gatsby-transformer-sharp`,
-		{
-			resolve: `gatsby-transformer-remark`,
-			options: {
-				plugins: [
-					`gatsby-remark-reading-time`,
-					`gatsby-remark-relative-images`,
-					{
-						resolve: `gatsby-remark-prismjs`,
-						options: {
-							noInlineHighlight: true,
-						}
-					},
-					{
-						resolve: `gatsby-remark-images`,
-						options: {
-							maxWidth: 750,
-							linkImagesToOriginal: false
-						}
-					}
-				]
-			}
-		},
-		{
-			resolve: `gatsby-plugin-google-fonts`,
-			options: {
-				fonts: [
-					'Inter',
-				],
-				display: 'swap'
-			}
-		},
-		{
-			resolve: `gatsby-plugin-google-analytics`,
-			options: {
-				trackingId: process.env.GA_TRACKING_ID
-			}
-		}
-	]
+    siteMetadata: {
+        title: `Andrea Diotallevi | Software Developer`,
+        author: `Andrea Diotallevi`,
+        description: `Andrea Diotallevi's personal website`,
+        url: `https://www.andreadiotallevi.com`,
+        image: `/andrea-diotallevi.jpg`,
+        twitterUsername: `@a_diotallevi_`,
+    },
+    plugins: [
+        `gatsby-plugin-react-helmet`,
+        `gatsby-plugin-sass`,
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                name: `src`,
+                path: `${__dirname}/src/`,
+            },
+        },
+        `gatsby-plugin-sharp`,
+        `gatsby-transformer-sharp`,
+        {
+            resolve: `gatsby-transformer-remark`,
+            options: {
+                plugins: [
+                    `gatsby-remark-reading-time`,
+                    `gatsby-remark-relative-images`,
+                    {
+                        resolve: `gatsby-remark-prismjs`,
+                        options: {
+                            noInlineHighlight: true,
+                        },
+                    },
+                    {
+                        resolve: `gatsby-remark-images`,
+                        options: {
+                            maxWidth: 750,
+                            linkImagesToOriginal: false,
+                        },
+                    },
+                ],
+            },
+        },
+        {
+            resolve: `gatsby-plugin-google-fonts`,
+            options: {
+                fonts: ["Inter"],
+                display: "swap",
+            },
+        },
+        {
+            resolve: `gatsby-plugin-google-analytics`,
+            options: {
+                trackingId: process.env.GA_TRACKING_ID,
+            },
+        },
+        {
+            resolve: `gatsby-plugin-mailchimp`,
+            options: {
+                endpoint:
+                    "https://andreadiotallevi.us7.list-manage.com/subscribe/post?u=7c92ef9d4158e1b488e3c25bf&amp;id=ab152c5061",
+                timeout: 3500, // number; the amount of time, in milliseconds, that you want to allow mailchimp to respond to your request before timing out. defaults to 3500
+            },
+        },
+    ],
 }
