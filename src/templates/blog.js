b01522506b04d1b7bb89131e960d2ac2c91e69a4
Andrea Diotallevi <andrea.diotallevi@outlook.com>
2021-09-25 15:03:37 +0200

    add newsletter form at the bottom

--- src/templates/blog.js
+++ src/templates/blog.js
@@ -4,62 +4,72 @@ import Img from "gatsby-image"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
+import Newsletter from "../components/newsletter"
 
 import blogStyles from "./blog.module.scss"
 
 export const query = graphql`
-	query (
-		$slug: String!
-	) {
-		markdownRemark (fields: { slug: { eq: $slug } }) {
-			frontmatter {
-				title
-				description
-				date(formatString: "MMMM Do, YYYY")
-				featuredImage {
-					childImageSharp {
-						fluid(maxWidth: 710) {
-							...GatsbyImageSharpFluid
-						}
-					}
-				}
-			}
-			html
-			fields {
-				readingTime {
-					text
-				}
-			}
-		}
-	}
+    query($slug: String!) {
+        markdownRemark(fields: { slug: { eq: $slug } }) {
+            frontmatter {
+                title
+                description
+                date(formatString: "MMMM Do, YYYY")
+                featuredImage {
+                    childImageSharp {
+                        fluid(maxWidth: 710) {
+                            ...GatsbyImageSharpFluid
+                        }
+                    }
+                }
+            }
+            html
+            fields {
+                readingTime {
+                    text
+                }
+            }
+        }
+    }
 `
 
-const Blog = (props) => {
-	const { title, description, date, featuredImage } = props.data.markdownRemark.frontmatter
+const Blog = props => {
+    const {
+        title,
+        description,
+        date,
+        featuredImage,
+    } = props.data.markdownRemark.frontmatter
 
-	return (
-		<Layout>
-			<Seo
-				title={title + " | Andrea Diotallevi"}
-				description={description}
-				image={featuredImage.childImageSharp.fluid.src}
-				article={true}
-			/>
-			<article className={blogStyles.container}>
-				<h1 className={blogStyles.title}>{title}</h1>
-				<div className={blogStyles.dateAndReadingTimeDiv}>
-					<p>{date}</p>
-					<p style={{ margin: "0 10px" }}>·</p>
-					<p>{props.data.markdownRemark.fields.readingTime.text}</p>
-				</div>
-				<Img fluid={featuredImage.childImageSharp.fluid} className={blogStyles.image} />
-				<div
-					dangerouslySetInnerHTML={{ __html: props.data.markdownRemark.html }}
-					className={blogStyles.blog}
-				/>
-			</article>
-		</Layout>
-	)
+    return (
+        <Layout>
+            <Seo
+                title={title + " | Andrea Diotallevi"}
+                description={description}
+                image={featuredImage.childImageSharp.fluid.src}
+                article={true}
+            />
+            <article className={blogStyles.container}>
+                <h1 className={blogStyles.title}>{title}</h1>
+                <div className={blogStyles.dateAndReadingTimeDiv}>
+                    <p>{date}</p>
+                    <p style={{ margin: "0 10px" }}>·</p>
+                    <p>{props.data.markdownRemark.fields.readingTime.text}</p>
+                </div>
+                <Img
+                    fluid={featuredImage.childImageSharp.fluid}
+                    className={blogStyles.image}
+                />
+                <div
+                    dangerouslySetInnerHTML={{
+                        __html: props.data.markdownRemark.html,
+                    }}
+                    className={blogStyles.blog}
+                />
+            </article>
+            <Newsletter />
+        </Layout>
+    )
 }
 
 export default Blog
