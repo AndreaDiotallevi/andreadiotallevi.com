be4544d76b870e0f40e1b89b3cf9b0a0f4f8de0f
Andrea Diotallevi <andrea.diotallevi@outlook.com>
2022-04-27 20:51:26 +0200

    amend newsletter styling

--- src/components/newsletter.tsx
+++ src/components/newsletter.tsx
@@ -29,47 +29,58 @@ const Newsletter = () => {
     }
 
     return (
-        <div className={newsletterStyles.container}>
-            <div>
-                <h2>Get my new blog posts to your inbox</h2>
-                <p>
-                    Every few months I write a new blog post about software
-                    development here, on my blog. It might be on JavaScript,
-                    React, GraphQL, Python, Django or personal development
-                    topics.
-                </p>
-                <p>
-                    My goal is to share my daily learnings as a software
-                    developer, with the hope this will help some of you out
-                    there!
-                </p>
-                <form onSubmit={handleSubmit} noValidate>
-                    <input
-                        type="text"
-                        name="firstName"
-                        placeholder="First name"
-                        value={firstName}
-                        onChange={e => setFirstName(e.target.value)}
-                    />
-                    <input
-                        type="email"
-                        name="email"
-                        placeholder="Email"
-                        value={email}
-                        onChange={e => setEmail(e.target.value)}
-                    />
-                    <button type="submit">Subscribe!</button>
-                    {result?.result === "error" ? (
-                        <p className={newsletterStyles.error}>
-                            {result.msg.includes("already subscribed")
-                                ? "The email you entered is already subscribed."
-                                : result.msg}
-                        </p>
-                    ) : null}
-                    {result?.result === "success" ? (
-                        <p className={newsletterStyles.success}>{result.msg}</p>
-                    ) : null}
-                </form>
+        <div
+            style={{
+                width: "100%",
+                borderTop: "1px solid rgb(235,235,235)",
+                display: "flex",
+                justifyContent: "center",
+            }}
+        >
+            <div className={newsletterStyles.container}>
+                <div>
+                    <h2>Get my new blog posts to your inbox</h2>
+                    <p>
+                        Every few months I write a new blog post about software
+                        development here, on my blog. It might be on JavaScript,
+                        React, GraphQL, Python, Django or personal development
+                        topics.
+                    </p>
+                    <p>
+                        My goal is to share my daily learnings as a software
+                        developer, with the hope this will help some of you out
+                        there!
+                    </p>
+                    <form onSubmit={handleSubmit} noValidate>
+                        <label>First name</label>
+                        <input
+                            type="text"
+                            name="firstName"
+                            value={firstName}
+                            onChange={e => setFirstName(e.target.value)}
+                        />
+                        <label>Email</label>
+                        <input
+                            type="email"
+                            name="email"
+                            value={email}
+                            onChange={e => setEmail(e.target.value)}
+                        />
+                        <button type="submit">Subscribe</button>
+                        {result?.result === "error" ? (
+                            <p className={newsletterStyles.error}>
+                                {result.msg.includes("already subscribed")
+                                    ? "The email you entered is already subscribed."
+                                    : result.msg}
+                            </p>
+                        ) : null}
+                        {result?.result === "success" ? (
+                            <p className={newsletterStyles.success}>
+                                {result.msg}
+                            </p>
+                        ) : null}
+                    </form>
+                </div>
             </div>
         </div>
     )
