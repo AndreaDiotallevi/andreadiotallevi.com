18a3ad79239810c3012d0b543cee042a8c09e799
Andrea Diotallevi <andrea.diotallevi@outlook.com>
2022-07-03 16:44:02 +0100

    add logo component

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,20 +1,24 @@
 import React from "react"
 import { Link } from "gatsby"
 
-// import MenuToggleButton from "./menuToggleButton"
 import routes from "../utils/routes"
 
 import * as navbarStyles from "./navbar.module.scss"
+import Logo from "./logo"
 
 type DataProps = {
     open: boolean
     onClick: () => void
+    color?: string
 }
 
-const Navbar = ({ open, onClick }: DataProps) => {
+const Navbar = (props: DataProps) => {
     return (
         <nav className={navbarStyles.navbar}>
-            <div>{!open && <Link to="/">andreadiotallevi</Link>}</div>
+            {/* <div>{!open && <Link to="/">andreadiotallevi</Link>}</div> */}
+            <Link to="/" style={{ display: "flex", alignItems: "center" }}>
+                <Logo color={props.color} />
+            </Link>
             <ul>
                 {routes.map(({ menuName, baseUrl }) => (
                     <li key={menuName}>
