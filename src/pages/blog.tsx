4a9f83c4c3b9c40a486a2741a8f557ef11835321
Andrea Diotallevi <andrea.diotallevi@outlook.com>
2022-09-11 16:23:59 +0200

    preload images

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -69,6 +69,7 @@ const BlogPage = ({ data: { allMarkdownRemark } }: PageProps<DataProps>) => {
                                                         .gatsbyImageData
                                                 }
                                                 alt={frontmatter.title}
+                                                loading="eager"
                                             />
                                         </div>
                                         <div>
