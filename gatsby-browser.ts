1aabcf062477b081ceb70b02b45f71d2f2dc4bab
Andrea Diotallevi <andrea.diotallevi@outlook.com>
2023-01-22 17:36:51 +0000

    create gatsby folder

--- gatsby-browser.ts
+++
@@ -1,8 +0,0 @@
-// require("prismjs/themes/prism-tomorrow.css")
-// require("prismjs/themes/prism.css")
-// require("prismjs/themes/prism-coy.css")
-// require("prismjs/themes/prism-twilight.css")
-
-import "@fontsource/inter/400.css"
-import "@fontsource/inter/500.css"
-import "@fontsource/inter/700.css"
--- src/global.d.ts
+++
@@ -1,7 +0,0 @@
-// necessary to make scss module work. See https://github.com/gatsbyjs/gatsby/issues/8144#issuecomment-438206866
-declare module "*.scss" {
-    const content: { [className: string]: string }
-    export = content
-}
-
-declare module "gatsby-plugin-mailchimp"
