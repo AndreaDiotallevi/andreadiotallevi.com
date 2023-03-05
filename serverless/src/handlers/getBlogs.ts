d5fe0aec7a524139118a7b0af5eb1097537b4892
Andrea Diotallevi <andrea.diotallevi@outlook.com>
2023-03-05 17:16:33 +0000

    add headers to solve CORS

--- serverless/src/handlers/getBlogs.ts
+++ serverless/src/handlers/getBlogs.ts
@@ -10,5 +10,10 @@ export const handler = async (): Promise<APIGatewayProxyResult> => {
     return {
         statusCode,
         body,
+        headers: {
+            "Access-Control-Allow-Headers": "Content-Type",
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "GET",
+        },
     }
 }
