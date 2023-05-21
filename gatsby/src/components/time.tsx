f041d65cbf3b70f0ee1eb392ff19a40b97206e63
Andrea Diotallevi <andrea.diotallevi@outlook.com>
2023-05-21 11:16:22 +0100

    reduce icon size

--- gatsby/src/components/time.tsx
+++ gatsby/src/components/time.tsx
@@ -9,8 +9,8 @@ type Props = {
 const Time = (props: Props) => {
     return (
         <svg
-            width="20px"
-            height="20px"
+            width="18px"
+            height="18px"
             viewBox="0 0 20 20"
             xmlns="http://www.w3.org/2000/svg"
             className={styles.svg}
