dd003a5b1d512ef31a6b136a592314133656af94
Andrea Diotallevi <andrea.diotallevi@outlook.com>
2023-05-29 17:40:54 +0100

    create loading spinner component

---
+++ gatsby/src/components/loadingSpinner.tsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+import * as loadingSpinnerStyles from "./loadingSpinner.module.scss"
+
+const LoadingSpinner = () => {
+    return <span className={loadingSpinnerStyles.loader}></span>
+}
+
+export default LoadingSpinner
