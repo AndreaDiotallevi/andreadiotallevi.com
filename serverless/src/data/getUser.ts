db3941d893bf241359c8a0ad01a50df1181548c1
Andrea Diotallevi <andrea.diotallevi@guardian.co.uk>
2024-08-20 14:32:26 +0300

    test: add log

--- serverless/src/data/getUser.ts
+++ serverless/src/data/getUser.ts
@@ -10,21 +10,30 @@ export const getUser = async ({ accessToken }: { accessToken: string }) => {
         const getUserCommand = new GetUserCommand({ AccessToken: accessToken })
 
         const response = await client.send(getUserCommand)
+        console.log(response)
 
         const userAttributes = response.UserAttributes
         if (!userAttributes) throw new Error("No user attributes")
 
-        const username = userAttributes.find((attribute) => attribute.Name === "sub")?.Value
+        const username = response.Username
+        console.log(username)
         const email = userAttributes.find((attribute) => attribute.Name === "email")?.Value
+        console.log(email)
 
         if (!username) throw new Error("No username")
         if (!email) throw new Error("No email")
 
         const user = new User({ id: username, email })
+        console.log(user)
 
         const command = new GetItemCommand({ TableName: process.env.TABLE_NAME, Key: user.key() })
+        console.log(user.key())
+        console.log("com", command)
 
         const response2 = await dynamodb.send(command)
+        console.log("res", response2)
+
+        console.log("item", response2.Item)
 
         return {
             user: userFromItem(response2.Item as UserItem),
